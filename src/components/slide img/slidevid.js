4155ff749588fa38f672e422a611d70530100377
Alejandro Andrade <alex.anso.17@gmail.com>
2019-10-18 11:42:42 -0500

    Adding some cards to slidelong, .json Modified

---
+++ src/components/slide img/slidevid.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class slidevid extends Component {
+    render() {
+        return (
+            <div>
+                <p>Hola</p>
+            </div>
+        )
+    }
+}
