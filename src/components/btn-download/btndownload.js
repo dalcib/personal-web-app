5ec23e56c716eb94082b778bf3e9d069cf6e5845
Alejandro Andrade <alex.anso.17@gmail.com>
2019-11-03 18:50:23 -0600

    finish JumpBaller Page With styles to the components like slideVideogame and btn downloads

--- src/components/btn-download/btndownload.js
+++ src/components/btn-download/btndownload.js
@@ -2,26 +2,28 @@ import React, { Component } from 'react'
 import GameMac from '../../downloadFiles/JumpBallerVersionFinal.zip'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faApple , faWindows } from '@fortawesome/free-brands-svg-icons';
+import './btndownload.css'
 
 export default class btndownload extends Component {
     
     
     render() {
         return (
-            <div>
+            <div className = 'download-container'>
+                <h2>Download</h2>
                 <ul> 
                     <li>
-                        <a href={GameMac} download>
-                                <FontAwesomeIcon icon ={faApple} size = '4x'></FontAwesomeIcon>
-                        </a>
-                        <p>download version 1.1 for mac</p>
+                            <a href={GameMac} download>
+                                <FontAwesomeIcon className = 'Icon' icon ={faApple} size = '4x'></FontAwesomeIcon>
+                            </a>
+                            <p>version 1.1 for mac</p>
                     </li>
                <br/>
                     <li>
-                        <a href={GameMac} download>
-                            <FontAwesomeIcon icon ={faWindows} size = '4x'></FontAwesomeIcon>
-                        </a>
-                        <p>download version 1.1 for Windows</p>
+                            <a href={GameMac} download>
+                            <FontAwesomeIcon className = 'Icon' icon ={faWindows} size = '4x'></FontAwesomeIcon>
+                            </a>
+                            <p>version 1.1 for Windows</p>
                     </li>
                </ul>
             </div>
